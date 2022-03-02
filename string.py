b36118a516839ff9deb598406f481608066d1d4c
orchamo <orchamo@gmail.com>
2022-03-02 17:47:04 +0200

    .

---
+++ string.py
@@ -0,0 +1,9 @@
+# if contains name pring name, if family name, print twice, if neither print welcome
+
+something = ""
+fname = input()
+if "or" in something:
+    print("or")
+elif "hamo" in something:
+    print("hamo hamo")
+else: print ("welcome")
